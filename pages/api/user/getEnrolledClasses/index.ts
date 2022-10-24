5bcdf16b3ce9bda748509e04325dbfbde4ce84ef
UKth <p.wilson0506@gmail.com>
2022-10-24 16:33:26 -0500

    :sparkles: add edge case, college match

--- pages/api/user/getEnrolledClasses/index.ts
+++ pages/api/user/getEnrolledClasses/index.ts
@@ -10,15 +10,43 @@ async function handler(
   res: NextApiResponse<ResponseType>,
   data?: {
     userId?: number;
+    collegeId?: number;
   }
 ) {
   const userId = data?.userId ?? 0;
+  const collegeId = data?.collegeId ?? 0;
 
   const {
     targetId,
   }: {
     targetId?: number;
   } = req.body;
+
+  if (userId === targetId) {
+    return res.status(400).json({
+      ok: false,
+      error: errorMessages.friend.addUserSelf,
+    });
+  }
+
+  const targetUser = await client.user.findUnique({
+    where: {
+      id: targetId,
+    },
+  });
+  if (!targetUser) {
+    return res.status(400).json({
+      ok: false,
+      error: errorMessages.user.userNotFound,
+    });
+  }
+
+  if (targetUser.collegeId !== collegeId) {
+    return res.status(400).json({
+      ok: false,
+      error: errorMessages.friend.collegeNotMatched,
+    });
+  }
 
   if (targetId && !isFriend(userId, targetId)) {
     return res.status(400).json({
