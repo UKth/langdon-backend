78a4d16324e5934d3f3618749e94ad16ec3d4ae6
UKth <p.wilson0506@gmail.com>
2022-11-10 02:03:37 -0600

    :bug: friend request - add update logic

--- pages/api/friend/createFriendRequest.ts
+++ pages/api/friend/createFriendRequest.ts
@@ -25,18 +25,40 @@ async function handler(
 
   const code = Math.floor(100000 + Math.random() * 900000);
 
-  const friendRequest = await client.friendRequest.create({
-    data: {
+  const request = await client.friendRequest.findUnique({
+    where: {
       createrId: userId,
-      code,
     },
   });
-
-  if (!friendRequest) {
-    return res.status(400).json({
-      ok: false,
-      error: errorMessages.friend.createFriendRequestFailed,
+  if (request) {
+    const updateFriendRequest = await client.friendRequest.update({
+      where: {
+        createrId: userId,
+      },
+      data: {
+        code,
+      },
     });
+
+    if (!updateFriendRequest) {
+      return res.status(400).json({
+        ok: false,
+        error: errorMessages.friend.createFriendRequestFailed,
+      });
+    }
+  } else {
+    const friendRequest = await client.friendRequest.create({
+      data: {
+        createrId: userId,
+        code,
+      },
+    });
+    if (!friendRequest) {
+      return res.status(400).json({
+        ok: false,
+        error: errorMessages.friend.createFriendRequestFailed,
+      });
+    }
   }
 
   return res.json({
