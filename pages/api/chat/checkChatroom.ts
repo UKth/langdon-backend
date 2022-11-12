24562e4e7ddf471cc35535e19ff0ce78c35fa311
UKth <p.wilson0506@gmail.com>
2022-11-12 03:12:12 -0600

    :sparkles: add checkChatroom api

---
+++ pages/api/chat/checkChatroom.ts
@@ -0,0 +1,50 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import {
+  handleDates,
+  ResponseType,
+  sendOnePush,
+  validBoard,
+} from "@libs/server/util";
+
+import { errorMessages } from "@constants";
+import withHandler from "@libs/server/withHandler";
+import { User } from "@prisma/client";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+  {
+    user,
+  }: {
+    user: User;
+  }
+) {
+  const {
+    targetId,
+  }: {
+    targetId?: number;
+  } = req.body;
+
+  if (!targetId) {
+    return res
+      .status(400)
+      .json({ ok: false, error: errorMessages.invalidParams });
+  }
+
+  const existingChatroom = await client.chatroom.findFirst({
+    where: {
+      AND: [
+        { members: { some: { id: user.id } } },
+        { members: { some: { id: targetId } } },
+      ],
+    },
+  });
+
+  return res.status(400).json({
+    ok: true,
+    chatroomExist: !!existingChatroom,
+  });
+}
+
+export default withHandler({ methods: ["POST"], handler });
