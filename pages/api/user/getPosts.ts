0a92f50e9faebceae8740ecc85a846ca0a5b2cf6
UKth <p.wilson0506@gmail.com>
2022-11-15 00:27:27 -0600

    :bug: forgot handleDates

--- pages/api/user/getPosts.ts
+++ pages/api/user/getPosts.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
-import { ResponseType } from "@libs/server/util";
+import { handleDates, ResponseType } from "@libs/server/util";
 
 import { errorMessages } from "@constants";
 import withHandler from "@libs/server/withHandler";
@@ -57,7 +57,7 @@ async function handler(
 
   return res.json({
     ok: true,
-    posts,
+    posts: handleDates(posts),
   });
 }
 
