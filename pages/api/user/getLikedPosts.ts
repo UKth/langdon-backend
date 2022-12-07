08da2f046f08c59f7006bd9aa659f0762ee327d6
UKth <p.wilson0506@gmail.com>
2022-12-07 16:40:30 -0600

    :sparkles: add getLikedPosts

---
+++ pages/api/user/getLikedPosts.ts
@@ -0,0 +1,69 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import { handleDates, ResponseType } from "@libs/server/util";
+
+import { errorMessages } from "@constants";
+import withHandler from "@libs/server/withHandler";
+import { User } from "@prisma/client";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>,
+  {
+    user,
+  }: {
+    user: User;
+  }
+) {
+  const {
+    lastId,
+  }: {
+    lastId?: number;
+  } = req.body;
+
+  const posts = await client.post.findMany({
+    where: {
+      likedUsers: {
+        some: {
+          id: user.id,
+        },
+      },
+    },
+    include: {
+      _count: {
+        select: {
+          likedUsers: true,
+          comments: true,
+        },
+      },
+      board: true,
+    },
+    orderBy: {
+      id: "desc",
+    },
+    ...(lastId
+      ? {
+          cursor: {
+            id: lastId,
+          },
+          skip: 1,
+        }
+      : {}),
+    take: 30,
+  });
+
+  if (!posts) {
+    return res.status(400).json({
+      ok: false,
+      error: errorMessages.post.postsLoadFailed,
+    });
+  }
+
+  return res.json({
+    ok: true,
+    posts: handleDates(posts),
+    lastId,
+  });
+}
+
+export default withHandler({ methods: ["POST"], handler });
